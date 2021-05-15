27279b37c785dbbd22fca6e4c9edba4c4093f6c5
Alejandro Schmidt <alemusho@gmail.com>
2021-05-14 21:59:26 -0300

    orai orai orai

--- src/components/SingleTweet.jsx
+++ src/components/SingleTweet.jsx
@@ -3,19 +3,23 @@ import logo from "../logo.svg";
 import DeleteBtn from "./DeleteBtn";
 import LikeBtn from "./LikeBtn";
 import "./RightSidebar.css";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import actions from "../redux/actions/tweetActions";
 
 function SingleTweet({ tweet }) {
   const [comment, setComment] = useState("");
+  const [isVisible, setIsVisible] = useState(true);
   const user = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
 
+  const userCommentModal = useRef();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // setIsVisible(!isVisible);
     dispatch(
       actions.updateComment(tweet._id, {
         text: comment,
@@ -78,7 +82,9 @@ function SingleTweet({ tweet }) {
                     <path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path>
                   </g>
                 </svg>
-                <span className="centro__numero">1</span>
+                <span className="centro__numero">
+                  {tweet.comments && tweet.comments.length}
+                </span>
               </li>
               <li
                 className="centro__li d-flex"
@@ -135,52 +141,75 @@ function SingleTweet({ tweet }) {
           </div>
         </div>
       </div>
-      <div
-        class="modal fade text-white"
-        id="commentModal"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content" style={{ background: "rgb(25, 39, 52)" }}>
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">
-                Haz un comentario
-              </h5>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body text-center">
-              <form id="form-comment" onSubmit={handleSubmit}>
-                <input
-                  className="bg-light text-dark"
-                  type="text"
-                  name="comment"
-                  maxLength="35"
-                  value={comment}
-                  onChange={(e) => setComment(e.target.value)}
-                />
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button className="btn btn-secondary" data-bs-dismiss="modal">
-                Cerrar
-              </button>
-              <button
-                type="submit"
-                form="form-comment"
-                class="follow-btn py-2 px-4 rounded-pill"
-              >
-                Enviar
-              </button>
+      {isVisible && (
+        <div
+          ref={userCommentModal}
+          class="modal fade text-white"
+          id="commentModal"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div
+              class="modal-content"
+              style={{ background: "rgb(25, 39, 52)" }}
+            >
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Haz un comentario
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                <form id="form-comment" onSubmit={handleSubmit}>
+                  <input
+                    className="bg-light text-dark form-control mb-3"
+                    type="text"
+                    name="comment"
+                    maxLength="35"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                  />
+                </form>
+
+                {tweet.comments &&
+                  tweet.comments.map((comment) => {
+                    return (
+                      <div key={comment._id}>
+                        <h3>
+                          {" "}
+                          <strong> @{comment.author}</strong>{" "}
+                        </h3>
+
+                        <p>{comment.text}</p>
+                      </div>
+                    );
+                  })}
+              </div>
+              <div class="modal-footer">
+                <button
+                  className="btn btn-secondary rounded-pill px-4 py-2"
+                  data-bs-dismiss="modal"
+                >
+                  Cerrar
+                </button>
+                <button
+                  type="submit"
+                  form="form-comment"
+                  class="follow-btn py-2 px-4 rounded-pill"
+                >
+                  Enviar
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
